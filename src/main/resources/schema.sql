1d938712dbf135eee62e7f28e88b6deb4388b06a
emuravjova <emuravjova@playtika.com>
2017-11-20 20:59:04 +0200

    9th HW: fixes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 
-DROP TABLE car;
+DROP TABLE IF EXISTS car;
 CREATE TABLE car (
   car_id         INTEGER PRIMARY KEY,
   number VARCHAR(20),
@@ -8,31 +8,31 @@ CREATE TABLE car (
   color VARCHAR(15)
 );
 
-DROP TABLE seller;
+DROP TABLE IF EXISTS seller;
 CREATE TABLE seller (
   seller_id         INTEGER PRIMARY KEY,
   name VARCHAR(20),
   contacts  VARCHAR(70)
  );
 
-DROP TABLE sale_propos;
+DROP TABLE IF EXISTS sale_propos;
 CREATE TABLE sale_propos (
   sale_id INTEGER PRIMARY KEY,
   car_id INTEGER NOT NULL,
   seller_id  INTEGER NOT NULL,
   price INTEGER check (price>0),
-  is_closed BOOLEAN,
+  deal_id INTEGER,
 FOREIGN KEY (car_id) REFERENCES car(car_id),
 FOREIGN KEY (seller_id) REFERENCES seller(seller_id),
 UNIQUE (car_id)
     );
 
-DROP TABLE deal;
+DROP TABLE IF EXISTS deal;
 CREATE TABLE deal (
   deal_id INTEGER PRIMARY KEY,
   sale_id INTEGER NOT NULL,
   price INTEGER check (price>0),
-  state INTEGER,
+  state ENUM('active', 'rejected', 'accepted'),
   date DATE,
 FOREIGN KEY (sale_id) REFERENCES sale_propos(sale_id)
     );
