037bd3671dba98c6470ac04771a901a62289a397
emuravjova <emuravjova@playtika.com>
2017-11-23 13:37:09 +0200

    9th HW: fixes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,36 +3,36 @@ DROP TABLE IF EXISTS seller;
 DROP TABLE IF EXISTS seller_propos;
 DROP TABLE IF EXISTS deal;
 
-CREATE TABLE car (
-  car_id         INTEGER PRIMARY KEY,
-  number VARCHAR(20),
-  brand  VARCHAR(20),
-  year VARCHAR(4),
-  color VARCHAR(15)
+CREATE TABLE IF NOT EXISTS car (
+  id BIGINT PRIMARY KEY,
+  number VARCHAR(20) NOT NULL,
+  brand  VARCHAR(20) NOT NULL,
+  year INTEGER NOT NULL,
+  color VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE seller (
-  seller_id         INTEGER PRIMARY KEY,
-  name VARCHAR(20),
-  contacts  VARCHAR(70)
- );
+CREATE TABLE IF NOT EXISTS seller (
+  id BIGINT PRIMARY KEY,
+  name VARCHAR(20) NOT NULL,
+  contacts  VARCHAR(70) NOT NULL
+);
 
-CREATE TABLE seller_propos (
-  sale_id INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS offer (
+  id BIGINT PRIMARY KEY,
   car_id INTEGER NOT NULL,
   seller_id  INTEGER NOT NULL,
-  price INTEGER check (price>0),
+  price INTEGER check (price > 0),
   deal_id INTEGER,
-FOREIGN KEY (car_id) REFERENCES car(car_id),
-FOREIGN KEY (seller_id) REFERENCES seller(seller_id),
-UNIQUE (car_id)
-    );
+FOREIGN KEY (car_id) REFERENCES car(id),
+FOREIGN KEY (seller_id) REFERENCES seller(id),
+UNIQUE (car_id, deal_id)
+);
 
-CREATE TABLE deal (
-  deal_id INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS deal (
+  id BIGINT PRIMARY KEY,
   sale_id INTEGER NOT NULL,
-  price INTEGER check (price>0),
+  price INTEGER check (price > 0),
   state ENUM('active', 'rejected', 'accepted'),
-  date DATE,
-FOREIGN KEY (sale_id) REFERENCES seller_propos(sale_id)
-    );
+  date DATE NOT NULL,
+FOREIGN KEY (sale_id) REFERENCES offer(id)
+);
--- src/test/java/com/playtika/automation/database/mydatabase/MydatabaseApplicationTests.java
+++ src/test/java/com/playtika/automation/database/mydatabase/MydatabaseApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MydatabaseApplicationTests {
+public class MyDatabaseApplicationTests {
 
 	@Test
 	public void contextLoads() {
